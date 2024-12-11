5c9613084f9f0f0839ba71c7bc09d3991493f239
Miche <miche@provable.com>
2024-12-11 12:38:44 -0800

    add new inputs and button

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,24 @@
 import Link from "next/link";
 import styles from "./Button.module.css"; // Import the CSS module
 
-const Button = ({ link, text, color }) => {
+const Button = ({ link, text, color, onClick }) => {
   // Use appropriate class names based on the color prop
   const buttonClass = `${styles.button} ${color === "green" ? styles.green : styles.pink}`;
 
+  if (link) {
+    // Render as a link if the link prop is provided
+    return (
+      <Link href={link}>
+        <a className={buttonClass}>{text}</a>
+      </Link>
+    );
+  }
+
+  // Render as a button if no link is provided
   return (
-    <Link href={link}>
-      <a className={buttonClass}>{text}</a>
-    </Link>
+    <button className={buttonClass} onClick={onClick} type="button">
+      {text}
+    </button>
   );
 };
 
